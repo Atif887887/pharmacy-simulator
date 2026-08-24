bbad6f4d93a7c87b12d6d0826b1875afb3397284
Atif887887 <atifrehman887@gmail.com>
2026-08-24 14:00:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,7 @@ if not api_key:
     st.stop()
 
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel('gemini-1.5-flash')
-
+model = genai.GenerativeModel('gemini-1.5-pro')
 # Bypassing strict medical safety filters for clinical simulation
 safety = [
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
